d95d212d5c2fdbeae45f6362427b90cdfe69f6eb
Patrick martins de lima <pattrickx@edu.unifor.br>
2022-06-06 22:09:21 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+
+app.get('/soma', function(req, res) {
+    const {a}=req.query
+    const {b}=req.query
+    
+    res.send(soma(+a,+b));
+});
+
+app.post('/somab', function(req, res) {
+    const {a,b} = req.body    
+    res.send(soma(+a,+b));
+});
+
+function soma(a, b) {
+    return a + b;
+  }
+
+var port = 9090;
+
+// iniciando o processo do servidor
+app.listen(port, function() {
+    console.log(`App de Exemplo escutando na porta http://localhost:${port}/`);
+});
